cc1ccf0073e9ce8bbe5cbba507a325a6092aeb86
snehal-77 <pawarsnehal1909@gmail.com>
2026-01-15 21:56:23 +0530

    Add hello world python file

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+name = "Snehal Rajendra Pawar"
+role = "Python Intern"
+today_date = date.today()
+
+print("Name:", name)
+print("Internship Role:", role)
+print("Date:", today_date)
