70dfd2114cd7cb90e11bf2ac82bd2bd4c1c27178
snehal-77 <pawarsnehal1909@gmail.com>
2026-01-15 22:41:06 +0530

    Add task1 python file

---
+++ task1.py
@@ -0,0 +1,10 @@
+from datetime import date
+
+name = input("Enter your name:")
+role = input("Enter your intership role:")
+
+today_date = date.today()
+
+print("Name:", name)
+print("Role:", role)
+print("Date:", today_date)
